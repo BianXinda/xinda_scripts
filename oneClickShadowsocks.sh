2aa891b6d6c4cba9f24dcfc5778b4048a1b38296
BianXinda <nus.bxd@gmail.com>
2016-05-28 23:19:05 +0800

    Add shadowsocks

--- oneClickShadowsocks.sh
+++ oneClickShadowsocks.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-#init checkin !
+
+echo -e "Please setup:\n------\nYour server ip address\nYour server port (default 8080)\nYour local port(default 1080)\nYour password(default 888888)\nYour timeout value(default 600)\nYour encrypt method(default aes-256-cfb)\n------\nbefore you run this script."
+exit #Remove this line when values are all setup
+
+server_ip="1.1.1.1" #At lease you need to change this value !
+server_port=8080
+local_port=1080
+password=888888
+timeout=600
+encrypt_method="aes-256-cfb"
+
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get python-gevent
+sudo apt-get python-pip
+sudo pip install shadowsocks
+sudo apt–get install python–m2crypto
+
+
+echo -e "{\n  \"server\":\"$server_ip\",\n  \"server_port\":$server_port,\n  \"local_port\":$local_port,\n  \"password\":\"$password\",\n  \"timeout\":$timeout,\n  \"method\":\"$encrypt_method\"\n}" > ~/config.json
+
+nohup ssserver –c ~/config.json  > ~/log &
+
+sudo echo /usr/local/bin/ssserver -c ~/config.json >> /etc/rc.local
